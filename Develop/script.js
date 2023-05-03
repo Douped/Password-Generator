a853d912e6f1b039d8460a510ae6c9e2888cd32c
Douped <anthony.rooyakkers2001@gmail.com>
2023-05-03 11:41:28 -0400

    Added promts for lowerCase upperCase numeric and special characters. Added arrays for each character type, added error checking for when user provides no input.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 //Math.floor(Math.random() * (testArray.length - 1)
 function generatePassword(){
+  var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  var numericArray = [1,2,3,4,5,6,7,8,9,0];
+  var specialArray = ["!","@","#","$","%","^","\&","*","(",")","-","_","+","=","`","~","[","]","{","}","/","?","<",">",",","."];
+  console.log(lowerCaseArray);
+
+  var password, lowerCase, upperCase, numeric, special;
   //Length prompts
   var passwordLength = window.prompt("Please enter a password length of at least 8 characters and at most 128 characters.");
   //if box is left empty
@@ -15,7 +21,19 @@ function generatePassword(){
   else if(passwordLength < 8 || passwordLength > 128){
     window.alert("Please enter an appropriate password length.");
   }
-  var password;
+  //prompt for including lowercase, uppercase, numeric, and/or special characters
+  else{
+    lowerCase = confirm("Would you like lower case characters in your password?");
+    upperCase = confirm("Would you like upper case characters in your password?");
+    numeric = confirm("Would you like numeric characters in your password?");
+    special = confirm("Would you like special characters in your password?");
+  }
+  //if user chooses no for all:
+  if(!(lowerCase && upperCase && numeric && special) && passwordLength != null){
+    window.alert("Password must contain SOME kind of characters!");
+  }else{
+    
+  }
 
   return password;
 }
