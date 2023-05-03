866850f3316545c54be9fdfe2ec3d4c9e007ae61
Douped <anthony.rooyakkers2001@gmail.com>
2023-05-03 15:22:57 -0400

    Modified password prompts to add required characters to a single array(rather than using math on multilpe different arrays). Modified upper case option by using an array (was originally going to use math with the toUpperCase function on the lowerCaseArray). added return password for incorrect prompt entries (so text box never reads undefined). implemented for loop and math to generate password.

--- Develop/script.js
+++ Develop/script.js
@@ -2,16 +2,20 @@
 //Math.floor(Math.random() * (testArray.length - 1)
 function generatePassword(){
   var lowerCaseArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  var numericArray = [1,2,3,4,5,6,7,8,9,0];
+  //learned the map function to save myself time
+  var upperCaseArray = lowerCaseArray.map(function(x){return x.toUpperCase();});
+  var numericArray = ["1","2","3","4","5","6","7","8","9","0"];
   var specialArray = ["!","@","#","$","%","^","\&","*","(",")","-","_","+","=","`","~","[","]","{","}","/","?","<",">",",","."];
-  console.log(lowerCaseArray);
+  var randomArray = [];
+  var password = "Your Secure Password";
+  var lowerCase, upperCase, numeric, special;
 
-  var password, lowerCase, upperCase, numeric, special;
   //Length prompts
   var passwordLength = window.prompt("Please enter a password length of at least 8 characters and at most 128 characters.");
   //if box is left empty
   if(passwordLength == null|| passwordLength == ""){
     window.alert("Please enter a password length.");
+    return password;
   }
   //if password is non-numerical
   else if(isNaN(passwordLength)){
@@ -23,19 +27,31 @@ function generatePassword(){
   }
   //prompt for including lowercase, uppercase, numeric, and/or special characters
   else{
-    lowerCase = confirm("Would you like lower case characters in your password?");
-    upperCase = confirm("Would you like upper case characters in your password?");
-    numeric = confirm("Would you like numeric characters in your password?");
-    special = confirm("Would you like special characters in your password?");
+    if(lowerCase = confirm("Would you like lower case characters in your password?")){
+      randomArray = randomArray.concat(lowerCaseArray);
+    }
+    if(upperCase = confirm("Would you like upper case characters in your password?")){
+      randomArray = randomArray.concat(upperCaseArray);
+    }
+    if(numeric = confirm("Would you like numeric characters in your password?")){
+      randomArray = randomArray.concat(numericArray);
+    }
+    if(special = confirm("Would you like special characters in your password?")){
+      randomArray = randomArray.concat(specialArray);
+    }
+    //if nothing is added to the array and no uppercase characters are allowed:
+    if(randomArray.length == 0){
+      window.alert("Password must contain at least one type of character")
+    }
+    //if promt is complete generate password
+    else{
+      password = '';
+      for(i = 0; i<passwordLength; i++){
+        password += randomArray[(Math.floor(Math.random() * (randomArray.length - 1)))];
+      }
+      return password;
+    }
   }
-  //if user chooses no for all:
-  if(!(lowerCase && upperCase && numeric && special) && passwordLength != null){
-    window.alert("Password must contain SOME kind of characters!");
-  }else{
-    
-  }
-
-  return password;
 }
 
 
